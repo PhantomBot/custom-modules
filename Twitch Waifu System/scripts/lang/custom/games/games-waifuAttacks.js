1042c98ff8c31e290871db8652a0b2cf28c9a8c2
Alex De Arnas <thephantomindex@gmail.com>
2017-05-04 12:55:09 -0400

    Massive Update
    Added Attributes:
    Attributes are randomly generated when seducing a character and will
    determine what attacks your character will do in !battle. You can change
    your character's attribute anytime with !attribute <number> <waifu>
    there are 10 total attributes at the moment.
    
    Added a lang for Attacks and attribute, when adding them it will
    automatically count the new information after a reboot.
    
    Rewritten the battle system math and improved the message style in lang
    to be easier to customize and change. !fight is now !battle.
    
    A whole lot of bug fixes.

---
+++ Twitch Waifu System/scripts/lang/custom/games/games-waifuAttacks.js
@@ -0,0 +1,70 @@
+$.lang.register('waifugames.attribute.success', '$1 you\'ve changed $2\'s attribute to $3!');
+$.lang.register('waifugames.attribute.404', '$1 attributes can only be set 1 through $2! 1=Normal, 2=Lewd, 3=Fighter, 4=Crazy, 5=Afraid, 6=LoveyDovey, 7=Sadistic.');
+$.lang.register('waifugames.attribute.usage', '$1 type !attribute <number> <waifu> to change your character\'s attribute!');
+
+$.lang.register('waifugames.attribute.1', 'Normal');
+$.lang.register('waifugames.attribute.2', 'Lewd');
+$.lang.register('waifugames.attribute.3', 'Fighter');
+$.lang.register('waifugames.attribute.4', 'Crazy');
+$.lang.register('waifugames.attribute.5', 'Afraid');
+$.lang.register('waifugames.attribute.6', 'Loveydovey');
+$.lang.register('waifugames.attribute.7', 'Sadistic');
+$.lang.register('waifugames.attribute.8', 'Magician');
+$.lang.register('waifugames.attribute.9', 'Klutz');
+$.lang.register('waifugames.attribute.10', 'DBZ');
+
+$.lang.register('waifugames.attack.1.1', 'threw a pillow at $1 [HP:$2]');
+$.lang.register('waifugames.attack.1.2', 'slapped $1\'s [HP:$2] face');
+$.lang.register('waifugames.attack.1.3', 'punched $1 [HP:$2] in the face');
+$.lang.register('waifugames.attack.1.4', 'kicked $1 [HP:$2] in the face');
+
+$.lang.register('waifugames.attack.2.1', 'licked $1\'s [HP:$2] most sensitive spots');
+$.lang.register('waifugames.attack.2.2', 'molested $1 [HP:$2]');
+$.lang.register('waifugames.attack.2.3', 'rubbed her breasts all over $1 [HP:$2]');
+$.lang.register('waifugames.attack.2.4', 'slapped $1 [HP:$2] with a onahole');
+$.lang.register('waifugames.attack.2.5', 'erotically hugged $1 [HP:$2]');
+$.lang.register('waifugames.attack.2.6', 'climaxed all over $1 [HP:$2]');
+
+$.lang.register('waifugames.attack.3.1', 'used Hadouken on $1 [HP:$2]');
+$.lang.register('waifugames.attack.3.2', 'performed a Dragon Punch on $1 [HP:$2]');
+$.lang.register('waifugames.attack.3.3', 'did a IAD combo on $1 [HP:$2]');
+$.lang.register('waifugames.attack.3.4', 'slammed $1 [HP:$2] into a V-Triggered Juggle combo');
+$.lang.register('waifugames.attack.3.5', 'Threw an arcade stick at $1 [HP:$2]');
+$.lang.register('waifugames.attack.3.5', 'activated the lag switch and started teleport punching the shit out of $1 [HP:$2]');
+
+
+$.lang.register('waifugames.attack.4.1', 'stabbed $1 [HP:$2] in the stomach multiple times');
+$.lang.register('waifugames.attack.4.2', 'slashed $1 [HP:$2] face using a knife filled with love');
+$.lang.register('waifugames.attack.4.3', 'attempted to saw off $1\'s [HP:$2] limbs');
+$.lang.register('waifugames.attack.4.4', 'electricuted $ [HP:$2]');
+$.lang.register('waifugames.attack.4.5', 'whipped $1 [HP:$2]');
+$.lang.register('waifugames.attack.4.6', 'stomped $1 [HP:$2]');
+
+$.lang.register('waifugames.attack.5.1', 'unleashed an explosive punch out of fear on $1 [HP:$2]');
+
+$.lang.register('waifugames.attack.6.1', 'kissed and hugged $1 [HP:$2]');
+$.lang.register('waifugames.attack.6.2', 'fed $1 [HP:$2] her homemade lunch');
+
+$.lang.register('waifugames.attack.7.1', 'beat $1 [HP: $2] until she bled');
+$.lang.register('waifugames.attack.7.2', 'tied $1 [HP: $2] in tight rope and beat $1 with a metal bat');
+$.lang.register('waifugames.attack.7.3', 'tied $1 [HP: $2] in tight rope and beat $1 with a plastic bat');
+$.lang.register('waifugames.attack.7.4', 'kicked, punched, and hugged $1 [HP: $2]');
+$.lang.register('waifugames.attack.7.5', 'kicked, punched, and snuggled $1 [HP: $2]');
+$.lang.register('waifugames.attack.7.6', 'dunked $1 [HP: $2] in hot wax');
+
+$.lang.register('waifugames.attack.8.1', 'chants EXPLOSION damaging $1 [HP: $2] and the area');
+$.lang.register('waifugames.attack.8.2', 'casted Shiny Ballista on $1 [HP: $2]');
+$.lang.register('waifugames.attack.8.3', 'casts Fallen Down on the helpless $1 [HP: $2]');
+$.lang.register('waifugames.attack.8.4', 'summons Ia Shub-Niggurath, creating a deadly black cyclone attacking $1 [HP: $2] and anything around her');
+$.lang.register('waifugames.attack.8.5', 'casts petrify on $1 [HP: $2] , turning her into stone(for about 2 mins)');
+
+$.lang.register('waifugames.attack.9.1', 'bumped against a book shielf, causing it to fall on $1 [HP: $2]');
+$.lang.register('waifugames.attack.9.2', 'slipped while cleaning and launched many plasic knives toward $1 [HP: $2]');
+$.lang.register('waifugames.attack.9.3', 'accidently tripped and pressed the nuke $1 [HP: $2] button');
+$.lang.register('waifugames.attack.9.4', 'dropped heavy books on $1 [HP: $2] pinky toe');
+
+$.lang.register('waifugames.attack.10.1', 'performed the Kamehameha wave on $1 [HP: $2]');
+$.lang.register('waifugames.attack.10.2', 'threw a massive Spirit Bomb on to $1 [HP: $2]');
+$.lang.register('waifugames.attack.10.3', 'used Instant Transmission on $1 [HP: $2] to teleport $1 into the sun');
+$.lang.register('waifugames.attack.10.4', 'threw $1 [HP: $2] into a crator and shot dozens of ki blasts destroying the area.');
+$.lang.register('waifugames.attack.10.5', 'used Kaio-ken X10 and summoned a flurry of kicks & punches onto $1 [HP: $2]');
